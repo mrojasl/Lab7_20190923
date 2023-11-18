f345a05598e5f009282d428885532233eb00162e
mrojasl <mrojasl@pucp.edu.pe>
2023-11-18 12:31:43 -0500

    commit final

--- app/src/main/java/com/example/lab7_20190923/MainActivity.java
+++ app/src/main/java/com/example/lab7_20190923/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.lab7_20190923;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+                new FirebaseAuthUIActivityResultContract(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK) {
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        Log.d("msg-test", "Firebase uid: " + user.getUid());
+                    } else {
+                        Log.d("msg-test", "Canceló el Log-in");
+                    }
+                }
+
+        );
+        Intent intent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(Arrays.asList(
+                        new AuthUI.IdpConfig.EmailBuilder().build(),
+                        new AuthUI.IdpConfig.GoogleBuilder().build()
+                ))
+                .build();
+
+        signInLauncher.launch(intent);
+
+
     }
+
+
 }
